74519a5f7c500a535c4dd3f7fca5afa36e9d6914
Ali Gholami <aligholami7596@gmail.com>
2019-02-16 10:29:48 +0330

    Update: Added start - end extraction

--- eeg_prep.py
+++ eeg_prep.py
@@ -1,11 +1,28 @@
 from mne import io
+import numpy as np
 import os
 
 import warnings
 warnings.filterwarnings("ignore")
 
+def get_class_positions(classes_dict, recording_types):
+
+    class_positions = {}
+
+    for klass_name, klass_id in classes_dict.items():
+        class_positions[klass_name] = np.argwhere(recording_types == klass_id)
+
+    return class_positions
+
 root = './data'
 classes = ['img_lhand', 'img_rhand', 'img_bfeet', 'img_tongue']
+
+classes_dict = {
+    'img_lhand': 769, 
+    'img_rhand': 770,
+    'img_bfeet': 771,
+    'img_tongue': 772
+}
 
 
 bci_record_names = []
@@ -27,8 +44,10 @@ recording_durations = recording_headers[4]
 # convert to dataframe
 recording_pd = gdf_file.to_data_frame()
 
-print("Recording positions: ", recording_positions)
-print("Recording durations: ", recording_durations)
-print("Recording types: ", recording_types)
+classes_positions = get_class_positions(classes_dict, recording_types)
 
 
+recording_idx = classes_positions['img_rhand'][0]
+start_time = recording_positions[recording_idx]
+end_time = start_time + recording_durations[recording_idx]
+print("One sample of class 1 is from {} to {}".format(start_time, end_time))
