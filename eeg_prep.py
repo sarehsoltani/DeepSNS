f6df2f341920f82ebcc91780a0d91832e2681b24
Ali Gholami <aligholami7596@gmail.com>
2019-02-16 17:49:00 +0330

    Update: Extracts basic classes from the signal

--- eeg_prep.py
+++ eeg_prep.py
@@ -1,5 +1,6 @@
 from mne import io
 import numpy as np
+import pandas as pd
 import os
 
 import warnings
@@ -14,14 +15,21 @@ def get_class_positions(classes_dict, recording_types):
 
     return class_positions
 
+def clear_label_columns(column):
+
+    column.replace(0, 'none', inplace=True)
+
+    print("Done replacing ")
+    return column
+
 root = './data'
-classes = ['img_lhand', 'img_rhand', 'img_bfeet', 'img_tongue']
+classes = ['img_lhand', 'img_rhand', 'img_bfeet', 'img_tongue', 'none']
 
 classes_dict = {
     'img_lhand': 769, 
     'img_rhand': 770,
     'img_bfeet': 771,
-    'img_tongue': 772
+    'img_tongue': 772,
 }
 
 
@@ -42,12 +50,27 @@ recording_types = recording_headers[2]
 recording_durations = recording_headers[4]
 
 # convert to dataframe
-recording_pd = gdf_file.to_data_frame()
+recording_ts = gdf_file.to_data_frame()         # Recording time-series
 
-classes_positions = get_class_positions(classes_dict, recording_types)
+classes_ids = get_class_positions(classes_dict, recording_types)
 
+# Add label column to recording ts dataframe
+new_col = pd.DataFrame(0, index=np.arange(recording_ts.shape[0]), columns=['class_label'])
 
-recording_idx = classes_positions['img_rhand'][0]
-start_time = recording_positions[recording_idx]
-end_time = start_time + recording_durations[recording_idx]
-print("One sample of class 1 is from {} to {}".format(start_time, end_time))
+for klass, klass_ids in classes_ids.items():
+    new_col.iloc[klass_ids] = klass
+
+new_col = clear_label_columns(new_col)
+
+print("New labels column: ", new_col)
+# 
+# recording_ts['class_label'] = pd
+# for klass, klass_ids in classes_ids.items():
+# 
+    # print("Class {0} IDs: {1}".format(klass, klass_ids))
+
+# print("Recording time-series: ", recording_ts)
+# recording_idx = classes_ids['img_rhand'][0]
+# start_time = recording_positions[recording_idx]
+# end_time = start_time + recording_durations[recording_idx]
+# print("One sample of class 1 is from {} to {}".format(start_time, end_time))
